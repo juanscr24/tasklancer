50379cd9bed8925405e0ff376883bad8a00fabae
Forlan Ordoñez <nalrofor@gmail.com>
2025-11-24 17:18:56 -0500

    feat: section navbar done + responsive

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,12 +2,12 @@ import Navbar from "@/components/features/Navbar";
 
 export default function Home() {
     return (
-        <main>
-            <header>
-                <div>
-                    <Navbar />
-                </div>
-            </header>
+        <main className="min-h-screen bg-(--bg-1) text-(--text-1) transition-colors duration-300">
+            <Navbar />
+
+            <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
+                {/* contenido del Hero */}
+            </section>
         </main>
     );
 }
--- src/components/features/Navbar.tsx
+++ src/components/features/Navbar.tsx
@@ -1,22 +1,69 @@
+'use client'
 
 import { LanguageSwitcher } from "../ui/LanguageSwitcher"
 import { ButtonMode } from "../ui/ButtonMode"
 import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/routing";
+import Image from "next/image";
+import { useTheme } from "@/utils/ThemeProvider";
 
 const Navbar = () => {
     const t = useTranslations('navbar');
+    const { theme } = useTheme()
+
     return (
-        <>
-            <div className="flex gap-4 p-4 bg-(--bg-1) h-30">
-                <ButtonMode />
-                <LanguageSwitcher className="relative" />
+        <header className="w-full sticky top-0 z-50 bg-(--bg-1) border-b border-(--text-2)/10 transition-colors duration-300">
+            <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-9">
+                <div className="flex items-center justify-between h-20">
+
+                    <div className="flex-shrink-0 flex items-center gap-2">
+                        <div className="h-12  flex items-center pt-2">
+                            <Image
+                                src={theme === 'dark' ? '/logo/logo_dm.png' : '/logo/logo_lm.png'}
+                                alt="Logo"
+                                width={200}
+                                height={200}
+                                loading="eager"
+                                className="object-cover"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="flex items-center gap-6">
+
+                        <nav className="hidden md:flex gap-6">
+                            <div className="flex items-center gap-3">
+                                <ButtonMode />
+                                <LanguageSwitcher className="relative" />
+                            </div>
+                        </nav>
+
+                        {/* Separador vertical */}
+                        <div className="hidden md:block h-6 w-px bg-(--text-2)/20"></div>
+
+                        <Link
+                            href="/contact"
+                            className="text-sm font-medium text-(--text-2) hover:text-(--text-1) transition-colors"
+                        >
+                            Contact
+                        </Link>
+
+                        <Link
+                            href="/services"
+                            className="text-sm font-medium text-(--text-2) hover:text-(--text-1) transition-colors"
+                        >
+                            Services
+                        </Link>
+                        <Link
+                            href="/auth"
+                            className="px-6 py-2.5 rounded-lg bg-(--btn-1) text-white font-semibold text-sm shadow-sm hover:opacity-90 transition-all active:scale-95"
+                        >
+                            Start Now
+                        </Link>
+                    </div>
+                </div>
             </div>
-            <div className="w-1/4">
-                <h1 className="">{t('welcome')}</h1>
-                <Link href="/auth">Start Now</Link>
-            </div>
-        </>
+        </header>
     )
 }
 
